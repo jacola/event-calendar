ac3b57d1bc28b443fd64cc3336ad089a229ef6e2
Jacques Lalancette <me@jacques.io>
2019-08-19 21:05:56 +0900

    Fix #8 - added styles back to calendar component.

--- client/src/components/CalendarHome.vue
+++ client/src/components/CalendarHome.vue
@@ -48,4 +48,32 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+  .red {
+    background: rgb(235, 77, 77) !important;
+    color: whitesmoke !important;
+  }
+  .blue {
+    background: rgb(59, 59, 163) !important;
+    color: whitesmoke !important;
+  }
+  .orange {
+    background: orange !important;
+    color: white !important;
+  }
+  .green {
+    background: rgb(49, 155, 49) !important;
+    color: white !important;
+  }
+  .blue,
+  .orange,
+  .red,
+  .green {
+    font-size: 13px;
+    font-weight: 500;
+    text-transform: capitalize;
+  }
+  .event-item {
+    padding: 2px 0 2px 4px !important;
+  }
+</style>
